949e73b71eba69c9529e211812c734ba56012117
Joel Almeida <joel.almeida.sa@gmail.com>
2024-06-13 22:55:54 -0300

    feat: Add comments section component
    The code changes introduce a new comments section component with its HTML, Sass, TypeScript, and test files. The component displays a section for comments, rendering individual comment components dynamically based on the data received from an observable. This enhances the reusability and flexibility of the application's comment functionality.

---
+++ src/app/components/comments-section/comments-section.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CommentsService, TComment } from '../../services/comment.service';
+import { CommentComponent } from '../comment/comment.component';
+
+@Component({
+  selector: 'app-comments-section',
+  standalone: true,
+  imports: [CommonModule, CommentComponent],
+  templateUrl: './comments-section.component.html',
+  styleUrl: './comments-section.component.sass'
+})
+export class CommentsSectionComponent {
+  constructor(private commentsService: CommentsService) { }
+
+  comments$!: Observable<TComment[]>;
+
+  ngOnInit(): void {
+    this.comments$ = this.commentsService.getCommentsTimeline();
+    console.log('Comments:', this.comments$);
+  }
+}
