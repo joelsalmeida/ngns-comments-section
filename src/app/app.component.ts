358953d28fbf74868710499dc62621b46bd7e652
Joel Almeida <joel.almeida.sa@gmail.com>
2024-06-13 22:57:15 -0300

    refactor: Update app.component.html and app.component.ts
    Refactor the app.component.html and app.component.ts files to replace the deprecated CommentComponent with the new CommentsSectionComponent. This change improves the clarity and consistency of the codebase by using a more descriptive and accurate component name.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommentComponent } from './components/comment/comment.component';
+import { CommentsSectionComponent } from './components/comments-section/comments-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommentComponent],
+  imports: [RouterOutlet, CommentsSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
