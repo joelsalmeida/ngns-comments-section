c159f65a1d1348a9078e559a493c43b4ed812893
Joel Almeida <joel.almeida.sa@gmail.com>
2024-06-15 18:22:00 -0300

    feat: Update post comment form to enable comment submission
    The code changes in this commit update the post comment form in the PostCommentComponent to enable comment submission. The form now includes a submit button with the type attribute set to "submit", allowing users to submit their comments by clicking the button. This enhancement improves the functionality of the application by providing a clear and intuitive way for users to submit their comments.

--- src/app/components/post-comment/post-comment.component.ts
+++ src/app/components/post-comment/post-comment.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommentsService } from '../../services/comment.service';
 import { ButtonComponent } from '../button/button.component';
 import { CardComponent } from '../card/card.component';
+
 
 @Component({
   selector: 'app-post-comment',
   standalone: true,
-  imports: [CardComponent, ButtonComponent],
+  imports: [CardComponent, ButtonComponent, FormsModule],
   templateUrl: './post-comment.component.html',
   styleUrl: './post-comment.component.sass'
 })
 export class PostCommentComponent {
+  constructor(private commentsService: CommentsService) { }
 
+  comment = { body: '', sender: 'baea2164-700d-42d4-bc57-08f3b2a19c03' }
+
+  postComment() {
+    this.commentsService.postComment(this.comment).subscribe(response => {
+      console.log('postComment response: ', response)
+    })
+  }
 }
