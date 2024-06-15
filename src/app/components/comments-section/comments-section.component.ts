2e98bac7b51848b0310758b4a1e95f09a0cdc657
Joel Almeida <joel.almeida.sa@gmail.com>
2024-06-15 12:02:13 -0300

    feat: Add Post Comment component to Comments Section

--- src/app/components/comments-section/comments-section.component.ts
+++ src/app/components/comments-section/comments-section.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CommentsService, TComment } from '../../services/comment.service';
 import { CommentComponent } from '../comment/comment.component';
+import { PostCommentComponent } from '../post-comment/post-comment.component';
 
 @Component({
   selector: 'app-comments-section',
   standalone: true,
-  imports: [CommonModule, CommentComponent],
+  imports: [CommonModule, CommentComponent, PostCommentComponent],
   templateUrl: './comments-section.component.html',
   styleUrl: './comments-section.component.sass'
 })
