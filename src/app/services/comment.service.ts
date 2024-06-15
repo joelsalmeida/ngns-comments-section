26156813e57339d177e1aad506481bf5b7a7a474
Joel Almeida <joel.almeida.sa@gmail.com>
2024-06-14 22:09:32 -0300

    feat: Refactor comment service and component
    The code changes refactor the comment service and component to improve functionality and user experience. The comment service now handles fetching comments from the server using the HttpClient module and includes error handling. The comment component is updated to display the comment body and interaction options dynamically, such as a like button and a reply button. These enhancements enhance the comments section component and make it more reusable and flexible.

--- src/app/services/comment.service.ts
+++ src/app/services/comment.service.ts
@@ -3,17 +3,31 @@ import { Injectable } from '@angular/core';
 import { Observable, catchError, map, of } from 'rxjs';
 import { environment } from '../../environments/environment';
 
-export type TComment = {
+
+
+export type TContent = {
   id: string,
   body: string,
   likes: string[],
   createdAt: string,
   updatedAt: string,
-  sender: {
-    id: string,
-    username: string
-  }
 }
+
+export type TUser = {
+  id: string,
+  username: string
+}
+
+export type TCommentResponse = {
+  sender: TUser,
+  recipient: TUser,
+} & TContent
+
+
+export type TComment = {
+  sender: TUser,
+  responses: TCommentResponse[],
+} & TContent
 
 type TResponse = {
   data: TComment[]
