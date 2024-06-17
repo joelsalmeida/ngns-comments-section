74fea54771dfefa106e3ec1f3c19cc6313d0437e
Joel Almeida <joel.almeida.sa@gmail.com>
2024-06-17 12:08:18 -0300

    refactor: Add "your" tag to mini-profile component
    The code changes in this commit modify the mini-profile component to display a "you" tag if the comment belongs to the current user. This enhancement improves the clarity and context of the comments by indicating the author of the comment.

--- src/app/components/mini-profile/mini-profile.component.ts
+++ src/app/components/mini-profile/mini-profile.component.ts
@@ -1,13 +1,15 @@
+import { NgIf } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-mini-profile',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './mini-profile.component.html',
   styleUrl: './mini-profile.component.sass'
 })
 export class MiniProfileComponent {
-  @Input() profilePic : string = '';
-  @Input() username : string = '';
+  @Input() profilePic: string = '';
+  @Input() username: string = '';
+  @Input() yourComment: boolean = false;
 }
