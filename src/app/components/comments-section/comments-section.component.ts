5dda846a1e03edcdc95ac352bf79786208a3c279
Joel Almeida <joel.almeida.sa@gmail.com>
2024-06-17 12:10:29 -0300

    refactor: Update comments section to include "yourComment" property on each comment

--- src/app/components/comments-section/comments-section.component.ts
+++ src/app/components/comments-section/comments-section.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
+import { AuthService } from '../../services/auth.service';
 import { CommentsService, TComment } from '../../services/comment.service';
 import { CommentComponent } from '../comment/comment.component';
 import { PostCommentComponent } from '../post-comment/post-comment.component';
@@ -13,9 +14,10 @@ import { PostCommentComponent } from '../post-comment/post-comment.component';
   styleUrl: './comments-section.component.sass'
 })
 export class CommentsSectionComponent {
-  constructor(private commentsService: CommentsService) { }
+  constructor(private commentsService: CommentsService, private authService: AuthService) { }
 
   comments$!: Observable<TComment[]>;
+  authenticatedUser = this.authService.authenticatedUser;
 
   ngOnInit(): void {
     this.comments$ = this.commentsService.comments;
