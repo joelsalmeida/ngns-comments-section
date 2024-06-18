10dd73392c3e4d391e9257faf131abbe31531d25
Joel Almeida <joel.almeida.sa@gmail.com>
2024-06-18 17:46:14 -0300

    refactor: Update like-button component to handle like and dislike functionality
    The code changes in this commit update the like-button component to handle both like and dislike functionality. The component now checks if the "liked" flag is set and applies the appropriate CSS class to the like and dislike buttons.

--- src/app/components/like-button/like-button.component.ts
+++ src/app/components/like-button/like-button.component.ts
@@ -1,12 +1,15 @@
+import { NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-like-button',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './like-button.component.html',
   styleUrl: './like-button.component.sass'
 })
 export class LikeButtonComponent {
-  @Input() likes : number = 0;
+  @Input() likes: number = 0;
+  @Input() liked: boolean = false;
+  @Input() onClick: () => void = () => { };
 }
