439b6ef236e226d3f4032238dbe53e9a70aef6e4
Joel Almeida <joel.almeida.sa@gmail.com>
2024-06-20 19:05:17 -0300

    chore: Add TimeAgoPipe for converting dates into human-readable format
    The code changes in this commit add a new TimeAgoPipe to the application. This pipe is used to convert a given date into a human-readable format, such as 'Just now', '1 minute ago', '2 hours ago', etc. The implementation of the pipe calculates the time difference between the current date and the provided date, and then formats it accordingly.

---
+++ src/app/pipes/time-ago.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TimeAgoPipe } from './time-ago.pipe';
+
+describe('DateAgoPipe', () => {
+  it('create an instance', () => {
+    const pipe = new TimeAgoPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ src/app/pipes/time-ago.pipe.ts
@@ -0,0 +1,42 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+/**
+ * TimeAgoPipe: Converts a date into a human-readable 'time ago' format.
+ * This implementation was inspired by a solution found on Stack Overflow.
+ * Reference: https://stackoverflow.com/a/61341940 (Accessed on June 20, 2024)
+ */
+@Pipe({
+  name: 'timeAgo',
+  pure: true,
+  standalone: true
+})
+export class TimeAgoPipe implements PipeTransform {
+
+  transform(value: any): any {
+    if (value) {
+      const seconds = Math.floor((+new Date() - +new Date(value)) / 1000);
+      if (seconds < 29) // less than 30 seconds ago will show as 'Just now'
+        return 'Just now';
+      const intervals: { [key: string]: number } = {
+        'year': 31536000,
+        'month': 2592000,
+        'week': 604800,
+        'day': 86400,
+        'hour': 3600,
+        'minute': 60,
+        'second': 1
+      };
+      let counter;
+      for (const i in intervals) {
+        counter = Math.floor(seconds / intervals[i]);
+        if (counter > 0)
+          if (counter === 1) {
+            return counter + ' ' + i + ' ago'; // singular (1 day ago)
+          } else {
+            return counter + ' ' + i + 's ago'; // plural (2 days ago)
+          }
+      }
+    }
+    return value;
+  }
+}
